6a2106c76e0525ae8c76d48531d15a2a1231fc9d
Jerry Zhao <jerryzhao@apple.com>
2024-03-09 22:55:40 +0800

    new photo

--- local.sh
+++ local.sh
@@ -1,5 +1,5 @@
 rm ./images/fulls/*
 rm ./images/locals/*
 cp ./images/local/* ./images/fulls/
-for file in ./images/local/*.jpg; do convert -resize 20% $file ./images/thumbs/"$(basename $file .jpg).jpg"; done
+for file in ./images/local/*.jpg; do convert -resize 10% $file ./images/thumbs/"$(basename $file .jpg).jpg"; done
 bundle exec jekyll serve
